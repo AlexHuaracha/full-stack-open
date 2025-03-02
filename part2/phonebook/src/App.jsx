6ecbd9fb1b7930814bd62b6fc710cef7589d717f
atipaq <alex.ronny.hq@gmail.com>
2025-03-02 10:23:16 -0500

    Part 2: phonebook step 2 Completed

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -9,11 +9,15 @@ const App = () => {
   const addPerson = (event) => {
     event.preventDefault()
 
-  
+    if (persons.find(person => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
 
     const personObject = {
       name: newName
     }
+    
     setPersons(persons.concat(personObject))
     setNewName('')
   }
