8288520a2e415f77b60070bc3047b36bfe29f12b
atipaq <alex.ronny.hq@gmail.com>
2025-03-04 10:33:50 -0500

    Part 2: phonebook step 11 Completed

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -57,12 +57,25 @@ const Persons = ({ persons, filter, setPersons }) => {
   )
 }
 
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null
+  }
+
+  return (
+    <div className='message'>
+      {message}
+    </div>
+  )
+}
+
 const App = () => {
 
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+  const [message, setMessage] = useState(null)
 
   useEffect(() => {
     personService
@@ -95,6 +108,10 @@ const App = () => {
         setPersons(persons.concat(returnedPerson))
         setNewName('')
         setNewNumber('')
+        setMessage(`Added ${returnedPerson.name}`)
+        setTimeout(() => {
+          setMessage(null)
+        }, 1500)
       })
   }
 
@@ -126,7 +143,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-
+      <Notification message={message} />
       <Filter filter={filter} handleFilterChange={handleFilterChange} />
 
       <h3>Add a new</h3>
