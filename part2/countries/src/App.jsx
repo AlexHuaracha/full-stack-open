bf4ba07830be751603ca8af9fb6c2a3e31e4b8c9
atipaq <alex.ronny.hq@gmail.com>
2025-03-09 10:42:30 -0500

    Part 2: countries step 3 Completed - All Completed

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,20 +1,45 @@
 import { useState, useEffect } from 'react'
 import countryService from './services/countries'
+import weatherService from './services/weathers'
 
-const CountryDetails = ({ country }) => (
-  <div>
-      <h2>{country.name.common}</h2>
-      <div>Capital {country.capital[0]}</div>
-      <div>Area {country.area}</div>
-      <h3>languages</h3>
-      <ul>
-        {Object.values(country.languages).map(language => 
-          <li key={language}>{language}</li>
+const CountryDetails = ({ country }) => {
+  const [weather, setWeather] = useState(null)
+  const countryCapital = country.capital[0]
+
+  useEffect(() => {
+    weatherService
+        .getWeather(countryCapital)
+        .then(data => {
+          // console.log('Weather data:', data)
+          setWeather(data)
+        })
+  }, [countryCapital])
+
+  // console.log('Icon', weather?.weather[0].icon)
+  // console.log('Weather:', countryCapital)
+  return (
+    <div>
+        <h2>{country.name.common}</h2>
+        <div>Capital {country.capital[0]}</div>
+        <div>Area {country.area}</div>
+        <h3>languages</h3>
+        <ul>
+          {Object.values(country.languages).map(language => 
+            <li key={language}>{language}</li>
+          )}
+        </ul>
+        <img src={country.flags.png} alt={country.flags.alt} width="177" />
+        {weather && (
+          <div>
+            <h3>Weather in {countryCapital}</h3>
+            <div>Temperature: {weather.main.temp} Celsius</div>
+            <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
+            <div>Wind: {weather.wind.speed} m/s</div>
+          </div>
         )}
-      </ul>
-      <img src={country.flags.png} alt={country.flags.alt} width="177" />
-    </div>
-)
+      </div>
+  )
+}
 
 const CountryList = ({ countries }) => {
   const [selectedCountry, setSelectedCountry] = useState(null);
@@ -39,7 +64,6 @@ const CountryList = ({ countries }) => {
           <button onClick={() => toggleCountry(country)}>
             {selectedCountry?.name.common === country.name.common ? "hide" : "show"}
           </button>
-          {/* Muestra detalles si el país está seleccionado */}
           {selectedCountry?.name.common === country.name.common && (
             <CountryDetails country={selectedCountry} />
           )}
@@ -68,13 +92,14 @@ const Country = ({countries, value}) => {
 function App() {
   const [value, setValue] = useState('')
   const [countries, setCountries] = useState([])
-    
+  
+
   useEffect(() => {
-    console.log('Fetching countries...')
+    // console.log('Fetching countries...')
     countryService
         .getAll()
         .then(data => {
-          console.log('Data received:', data)
+          // console.log('Data received:', data)
           setCountries(data)
         })
   }, [])
