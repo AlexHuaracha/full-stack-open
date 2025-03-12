20bf9fa2321d0c9441b26d7129f9bead87cf5ec6
atipaq <alex.ronny.hq@gmail.com>
2025-03-12 18:57:31 -0500

    Reorganize files to frontend and backend files

---
+++ part3/phonebook/frontend/src/App.jsx
@@ -0,0 +1,182 @@
+import { useState, useEffect } from 'react'
+import personService from './services/persons'
+import Person from './components/Person'
+
+const Filter = ({ filter, handleFilterChange }) => {
+  return (
+    <div>
+      filter shown with <input value={filter} onChange={handleFilterChange}/>
+    </div>
+  )
+}
+
+const PersonForm = ({ addPerson, newName, handleNameChange, newNumber, handleNumberChange }) => {
+  return (
+    <form onSubmit={addPerson}>
+      <div>
+        name: <input value={newName} onChange={handleNameChange} />
+      </div>
+      <div>
+        number: <input value={newNumber} onChange={handleNumberChange} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  )
+}
+
+const Persons = ({ persons, filter, setPersons }) => {
+  const personsToShow = persons.filter(
+    person => person.name.toLowerCase().includes(filter.toLowerCase())
+  )
+
+  const deletePerson = (id) => {
+    if (confirm(`Delete ${persons.find(person => person.id === id).name} ?`)) {
+      personService
+        .deletePerson(id)
+        .then(() => {
+          setPersons(persons.filter(person => person.id !== id))
+        })
+    }
+  }
+
+  return (
+    <div>
+      {
+        personsToShow.map(person => 
+          <Person 
+            key={person.id} 
+            name={person.name} 
+            number={person.number} 
+            deletePerson={() => deletePerson(person.id)}
+          />
+        )
+      }
+    </div>
+  )
+}
+
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null
+  }
+
+  return (
+    <div className={message.type === 'success' ? 'success' : 'error'}>
+      {message.text}
+    </div>
+  )
+}
+
+const App = () => {
+
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+  const [message, setMessage] = useState(null)
+
+  useEffect(() => {
+    personService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
+      })
+  }, [])
+  
+  const addPerson = (event) => {
+    event.preventDefault()
+
+    if (persons.find(person => person.name === newName)) {
+      if (confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
+        const id = persons.find(person => person.name === newName).id
+        updatePersonNumber(id)
+        return
+      }
+    }
+
+    const personObject = {
+      name: newName,
+      number: newNumber,
+      // id: persons.length + 1
+    }
+
+    personService
+      .create(personObject)
+      .then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson))
+        setNewName('')
+        setNewNumber('')
+        setMessage({
+          text: `Added ${returnedPerson.name}`,
+          type: 'success'
+        })
+        setTimeout(() => {
+          setMessage(null)
+        }, 1500)
+      })
+  }
+
+  const updatePersonNumber = (id) => {
+    const person = persons.find(person => person.id === id)
+    const changedPerson = { ...person, number: newNumber }
+
+    personService
+      .update(id, changedPerson)
+      .then(returnedPerson => {
+        setPersons(persons.map(person => person.id !== id ? person : returnedPerson))
+        setNewName('')
+        setNewNumber('')
+      })
+      .catch(error => {
+        setMessage({
+          text: `Information of ${person.name} has already been removed from server`,
+          type: 'error'
+        })
+        setTimeout(() => {
+          setMessage(null)
+        }, 1500)
+        setPersons(persons.filter(person => person.id !== id))
+      })
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  }
+
+  console.log('props message', message);
+  
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Notification message={message} />
+      <Filter filter={filter} handleFilterChange={handleFilterChange} />
+
+      <h3>Add a new</h3>
+
+      <PersonForm
+        addPerson={addPerson}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+        setPersons={setPersons}
+      />
+      
+      <h3>Numbers</h3>
+      <Persons persons={persons} filter={filter} setPersons={setPersons} />
+      
+    </div>
+  )
+}
+
+export default App
