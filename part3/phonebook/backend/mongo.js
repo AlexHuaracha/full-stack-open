421d4c9e4ef2dd1d2685a0e03888200221e4cdf5
atipaq <alex.ronny.hq@gmail.com>
2025-03-13 16:58:43 -0500

    Exercise 3.12: Command-line database - Completed

---
+++ part3/phonebook/backend/mongo.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.g4ujl.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model('Phonebook', personSchema)
+
+const person = new Person({
+    name: name,
+    number: number,
+})
+
+// Save the person to the database
+if (process.argv.length > 4) {
+    person.save().then(result => {
+        console.log(`added ${name} number ${number} to phonebook`)
+        mongoose.connection.close()
+    })
+}
+
+// Fetch all persons from the database
+if (process.argv.length < 4) {
+    Person.find({}).then(result => {
+        console.log('phonebook:')
+        result.forEach(person => {
+            console.log(person.name, person.number)
+        })
+        mongoose.connection.close()
+    })    
+}
+
+
+
+// 36QhYGq9SalH3s1G
