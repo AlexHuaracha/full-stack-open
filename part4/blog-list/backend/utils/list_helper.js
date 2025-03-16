f157efe12e8009a73660dd94faf968f96bc0d69e
atipaq <alex.ronny.hq@gmail.com>
2025-03-16 11:17:52 -0500

    4.5*: Helper Functions and Unit Tests, step 3 - Completed

--- part4/blog-list/backend/utils/list_helper.js
+++ part4/blog-list/backend/utils/list_helper.js
@@ -6,7 +6,20 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null
+  }
+  const favorite = blogs.reduce((favorite, blog) => favorite.likes > blog.likes ? favorite : blog, blogs[0])
+  return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes
+  }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
