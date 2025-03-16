a05d9e1258078efd4843b0fa86fca7ec3dc10829
atipaq <alex.ronny.hq@gmail.com>
2025-03-16 09:51:59 -0500

    Exercises 4.1 - 4.2 - Completed

---
+++ part4/blog-list/backend/controllers/blogs.js
@@ -0,0 +1,41 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+blogsRouter.post('/', (request, response, next) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+    .catch(error => next(error))
+})
+
+module.exports = blogsRouter
+
+// blogsRouter.get('/api/blogs', (request, response) => {
+//   Blog
+//     .find({})
+//     .then(blogs => {
+//       response.json(blogs)
+//     })
+// })
+
+// blogsRouter.post('/api/blogs', (request, response) => {
+//   const blog = new Blog(request.body)
+
+//   blog
+//     .save()
+//     .then(result => {
+//       response.status(201).json(result)
+//     })
+// })
