58e6926277b9b65103b7aca60c0f67f39d84087d
atipaq <alex.ronny.hq@gmail.com>
2025-04-16 19:57:12 -0500

    5.1 - 5.2: Blog List Frontend, step 1 and 2 - Completed

---
+++ part5/bloglist-frontend/src/App.jsx
@@ -0,0 +1,161 @@
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+  const [likes, setLikes] = useState('')
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+  
+  useEffect(() => {    
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )  
+  }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+  
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      )
+
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      console.error('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedBlogappUser')
+    setUser(null)
+  }
+
+  const loginForm = () => {
+    return (
+      <form onSubmit={handleLogin}>
+          <div>
+            username
+            <input
+              type="text"
+              value={username}
+              name="Username"
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </div>
+          <div>
+            password
+            <input
+              type="password"
+              value={password}
+              name="Password"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </div>
+          <button type="submit">login</button>
+        </form>
+    )
+  }
+
+  const blogForm = () => {
+    return (
+      <form>
+        <div>
+          title
+          <input
+            type="text"
+            value={title}
+            name="Title"
+            onChange={({ target }) => setTitle(target.value)}
+          />
+        </div>
+        <div>
+          author
+          <input
+            type="text"
+            value={author}
+            name="Author"
+            onChange={({ target }) => setAuthor(target.value)}
+          />
+        </div>
+        <div>
+          url
+          <input
+            type="text"
+            value={url}
+            name="Url"
+            onChange={({ target }) => setUrl(target.value)}
+          />
+        </div>
+        <div>
+          likes
+          <input
+            type="number"
+            value={likes}
+            name="Likes"
+            onChange={({ target }) => setLikes(target.value)}
+          />
+        </div>
+        <div>
+          user
+          <input
+            type="text"
+            value={user}
+            name="User"
+            readOnly
+          />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    )
+  }
+
+  return (
+    <div>
+      <h2>blogs</h2>
+
+      {!user && loginForm()}
+      {user && <div>
+        <p>{user.name} logged in</p>
+        <button onClick={handleLogout}>logout</button>
+        <h3>create new</h3>
+        {blogForm()}
+        </div>
+      }
+
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+
+      
+      
+    </div>
+  )
+}
+
+export default App
