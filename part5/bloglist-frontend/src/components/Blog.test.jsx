c4eb0c2779d95bd09a6364052bc760fcc4a1df65
atipaq <alex.ronny.hq@gmail.com>
2025-04-18 11:07:34 -0500

    5.14: Blog List Tests, step 2 - Done

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -40,3 +40,31 @@ test('clicking the button calls event handler once', async () => {
   expect(mockHandler.mock.calls).toHaveLength(0)
 
 })
+
+test('URL and likes are shown when the view button is clicked', async () => {
+  const blog = {
+    title: 'Test Blog',
+    author: 'Test Author',
+    url: 'http://testblog.com',
+    likes: 10,
+  }
+
+  render(<Blog blog={blog} />)
+  const user = userEvent.setup()
+
+  let urlElement = screen.queryByText('http://testblog.com')
+  let likesElement = screen.queryByText(/likes/i)
+  expect(urlElement).toBeNull()
+  expect(likesElement).toBeNull()
+
+  const viewButton = screen.getByText('view')
+  await user.click(viewButton)
+
+  urlElement = screen.getByText('http://testblog.com')
+  likesElement = screen.getByText((content, element) => {
+    return content.toLowerCase().includes('likes') && content.includes('10')
+  })
+
+  expect(urlElement).toBeInTheDocument()
+  expect(likesElement).toBeInTheDocument()
+})
